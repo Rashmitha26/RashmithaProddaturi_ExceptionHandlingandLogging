8a61d5a107fb230042fcd7b72761950d85797a71
Rashmitha26 <rashmithaproddaturi@gmail.com>
2020-02-10 22:19:44 +0530

    first commit

---
+++ calculator/src/calculator/CalculatorData.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class CalculatorData {
+	int op1,op2;
+	String op;
+	CalculatorData(int i1,int i2,String operand){
+		this.op1=i1;
+		this.op2=i2;
+		this.op=operand;
+	}
+
+}
